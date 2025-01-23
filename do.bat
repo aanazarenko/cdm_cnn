ad0a7d2266c13edcb1ec1a7868c558575fc47c1f
Alexander A. Nazarenko <132454219+aanazarenko@users.noreply.github.com>
2025-01-23 13:06:18 +0200

    [changed] --linear_input

--- do.bat
+++ do.bat
@@ -16,9 +16,9 @@ for %%F in ("%input_file%") do (
 )
 
 :: Generate the output file path
-set "output_file=%file_dir%%file_name%_cdmcnn%file_ext%"
+set "output_file=%file_dir%%file_name%__cdmcnn(lin)%file_ext%"
 
 :: Run the Python script with the input and output file paths
-python cdmcnn.py --offset_x=1 --offset_y=1 --input "%input_file%" --output "%output_file%"
+python cdmcnn.py --linear_input --offset_x=1 --offset_y=0 --input "%input_file%" --output "%output_file%"
 
 "exiftool(-k).exe" -tagsfromfile "%input_file%" -overwrite_original "%output_file%"
