37f5b39270541d5a37b3691bde632e6253c5334f
Alexander A. Nazarenko <132454219+aanazarenko@users.noreply.github.com>
2025-01-29 17:56:43 +0200

    [changed] output to gamma 2.2

--- do.bat
+++ do.bat
@@ -15,7 +15,7 @@ for %%F in ("%input_file%") do (
     set "file_ext=%%~xF"
 )
 
-set "dcraw_exe=dcraw -v -w -S 4071 -W -q 3 -o 5 -6 -g 1 0 -T"
+set "dcraw_exe=dcraw -v -w -S 1647 -W -q 3 -o 5 -6 -g 2.2 0 -T"
 
 %dcraw_exe%  "%input_file%"
 
@@ -35,6 +35,6 @@ echo "%photo_date%"
 set "output_file=%file_dir%%photo_date%_%file_name%__%dcraw_exe_X%__cdmcnn(lin).tiff"
 
 :: Run the Python script with the input and output file paths
-python cdmcnn.py --linear_input --offset_x=1 --offset_y=0 --input "%file_dir%%file_name%.tiff" --output "%output_file%"
+python cdmcnn.py --offset_x=0 --offset_y=1 --input "%file_dir%%file_name%.tiff" --output "%output_file%"
 
 "%exiftool_path%" -tagsfromfile "%input_file%" -overwrite_original "%output_file%"
